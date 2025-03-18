0f120bfb8d98fd47428aa30d3b9313dc86179043
Berkant Gunes <gunesberkant96@gmail.com>
2025-03-18 19:26:54 +0300

    Add files via upload

---
+++ Diagnosis.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hospitalmanagement
+{
+    public partial class Diagnosis: Form
+    {
+        public Diagnosis()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoctors_Click(object sender, EventArgs e)
+        {
+            Doctors doc = new Doctors();
+            doc.Show();
+            this.Hide();
+        }
+
+        private void btnPatients_Click(object sender, EventArgs e)
+        {
+            Patients ptn = new Patients();
+            ptn.Show();
+            this.Hide();
+        }
+
+        private void btnMedicine_Click(object sender, EventArgs e)
+        {
+            Medicine mdc = new Medicine();
+            mdc.Show();
+            this.Hide();
+        }
+
+        private void logoutbtn_Click(object sender, EventArgs e)
+        {
+            Admin_Login admn = new Admin_Login();
+            admn.Show();
+            this.Hide();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Medicine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hospitalmanagement
+{
+    public partial class Medicine: Form
+    {
+        public Medicine()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoctors_Click(object sender, EventArgs e)
+        {
+            Doctors doc = new Doctors();
+            doc.Show();
+            this.Hide();
+        }
+
+        private void btnPatients_Click(object sender, EventArgs e)
+        {
+            Patients ptn = new Patients();
+            ptn.Show();
+            this.Hide();
+        }
+
+        private void btnDiagnosis_Click(object sender, EventArgs e)
+        {
+            Diagnosis dgn = new Diagnosis();
+            dgn.Show();
+            this.Hide();
+        }
+
+        private void logoutbtn_Click(object sender, EventArgs e)
+        {
+            Admin_Login adm = new Admin_Login();
+            adm.Show();
+            this.Hide();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
